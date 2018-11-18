52fbb4f2a6ba49e7955cda12cebbe567e2274389
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2018-11-18 18:56:24 -0500

    need to make better

--- Tipulator/ViewController.swift
+++ Tipulator/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSelect: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    
+    }
+    @IBAction func calcTip(_ sender: Any) {
+        let tipPercents = [0.10, 0.15, 0.20]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercents[tipSelect.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)    }
+    }
 
-}
+    
+
 
