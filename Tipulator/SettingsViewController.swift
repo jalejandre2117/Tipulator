2887fdb73d93c1d77f37fab259335c83a3cc3d8b
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2019-11-08 12:14:46 -0500

    added steppers

---
+++ Tipulator/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  Tipulator
+//
+//  Created by Jorge Alejandre on 11/8/19.
+//  Copyright © 2019 Jorge Alejandre. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Tipulator"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tipulator/ViewController.swift
+++ Tipulator/ViewController.swift
@@ -9,32 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var personLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipSelect: UISegmentedControl!
+    @IBOutlet weak var tipStepper: UIStepper!
+    @IBOutlet weak var tipValueLabel: UILabel!
+    @IBOutlet weak var personStepper: UIStepper!
+    @IBOutlet weak var personValueLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tipStepper.wraps = true
+        tipStepper.autorepeat = true
+        tipStepper.minimumValue = 10
+        tipStepper.maximumValue = 25
+        
+        personStepper.wraps = true
+        personStepper.autorepeat = true
+        personStepper.minimumValue = 1
+        personStepper.maximumValue = 10
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func tipStepperValue(_ sender: UIStepper) {
+        tipValueLabel.text = "\(Double(sender.value).description)%" 
+        calculate()
+    }
+    
+    
+    @IBAction func personStepperValue(_ sender: UIStepper) {
+        personValueLabel.text = Double(sender.value).description
+        calculate()
+    }
+    
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
-    
-    }
-    @IBAction func calcTip(_ sender: Any) {
-        let tipPercents = [0.10, 0.15, 0.20]
         
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        calculate()
+    }
+    
+    func calculate() {
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercents[tipSelect.selectedSegmentIndex]
+        
+        let tip = (bill * (tipStepper.value)) / 100
+        let personTip = tip / (personStepper.value)
         let total = bill + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)    }
+        personLabel.text = String(format: "$%.2f", personTip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
+}
 
-    
+
 
 
